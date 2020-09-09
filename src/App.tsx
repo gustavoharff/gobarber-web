2925dc9e6afcea88a387472ac5610a3e42deaafa
Gustavo <gustavo.harff@gmail.com>
2020-09-09 15:17:49 -0300

    feat: toast hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,16 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
-import ToastContainer from './components/ToastContainer';
-
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks';
 
 const App: React.FC = () => {
   return (
     <>
-      <AuthProvider>
+      <AppProvider>
         <SignIn />
-      </AuthProvider>
-
-      <ToastContainer />
+      </AppProvider>
 
       <GlobalStyle />
     </>
