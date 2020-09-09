3ff109e369a3a5c644cb26be0768c95bc3aac692
Gustavo <gustavo.harff@gmail.com>
2020-09-09 16:20:25 -0300

    feat: animated toasts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -54,7 +54,7 @@ const SignIn: React.FC = () => {
         addToast({
           type: 'error',
           title: 'Erro na autenticação',
-          description: 'Occoreu um erro ao fazer login, cheque as credenciais.',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
         });
       }
     },
